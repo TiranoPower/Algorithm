1cc9172a7a7836c538eb0fe0738d8f1fa0548aa2
변준우 <122006463+TiranoPower@users.noreply.github.com>
2023-01-09 17:15:57 +0900

    Update DAY4.안경이없어.py

--- SWEA/DAY4.안경이없어.py
+++ SWEA/DAY4.안경이없어.py
@@ -1,33 +1,33 @@
 t = int(input()) ## 테스트케이스 초기화
-zero  = ['C', 'E', 'F', 'G', 'H', 'I','J','K','L','M','N','S','T','U','V','W','X','Y','Z'] # zero의 구멍 안난 횟수 리스트 저장 
-one = ['A','D','O','P','Q','R']
-# one의 구멍 1개 난 리스트 저장 
+
+zero  = ['C', 'E', 'F', 'G', 'H', 'I','J','K','L','M','N','S','T','U','V','W','X','Y','Z'] # zero의 구멍 안난 횟수의 리스트 저장 
+one = ['A','D','O','P','Q','R'] # one의 구멍 1개 난 횟수의 리스트 저장 
 
 for tc in range (1, t+1) : # 테스트케이스 1부터 입력한 값까지
   str1, str2 = input().split()  # 비교할 문자열 두개 입력
   arr1 = [] ## 첫번째의 리스트 초기화
   arr2 = [] ## 두번째의 리스트 초기화
 
-  for i in str1: ## 입력한 문자열 첫번째 리스트에 추가
-   arr1.append(i)
+  for i in str1: 
+   arr1.append(i) # 입력한 문자열 첫번째 리스트에 추가
 
-  for j in str2: ## 입력한 문자열 두번째 리스트에 추가
-     arr2.append(j)
+  for j in str2:
+     arr2.append(j)  # 입력한 문자열 두번째 리스트에 추가
 
   for k in range(len(arr1)):  # 첫번째 리스트 끝까지
-    if arr1[k] not in zero: # zero 리스트에 일치되는 값이 없다면 '1'값 저장
-      arr1[k] = 1
-    else :  # 있다면 '0'값 저장
-         arr1[k] = 0
+    if arr1[k] not in zero:  # 만약 zero 리스트에 일치되는 값이 없다면 '1'값 저장
+      arr1[k] = 1        
+    else : 
+         arr1[k] = 0  # 있다면 '0'값 저장
 
   for a in range(len(arr2)): # 두번째 리스트 끝까지
-    if arr2[a] not in zero :  # zero 리스트에 일치되는 값이 없다면 '1'값 저장
+    if arr2[a] not in zero :  # 만약 zero 리스트에 일치되는 값이 없다면 '1'값 저장
       arr2[a] = 1
     else : 
       arr2[a] = 0 # 있다면 '0'값 저장
        
-  if arr1 != arr2: # 첫번째 리스트와 두번째 리스트의 값이 다르면 'DIFF' 출력
-      print("#{} DIFF".format(tc))  
-  else : # 아니면 'SAME' 출력
-      print("#{} SAME".format(tc))  
+  if arr1 != arr2:
+      print("#{} DIFF".format(tc))   # 만약 첫번째 리스트와 두번째 리스트의 값이 다르면 'DIFF' 출력
+  else : 
+      print("#{} SAME".format(tc))   # 아니면 'SAME' 출력
   
