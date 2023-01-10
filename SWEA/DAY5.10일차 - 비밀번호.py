bb8cda51a087e538cdd8c78ad45548f8d16c1c27
변준우 <122006463+TiranoPower@users.noreply.github.com>
2023-01-10 16:08:53 +0900

    Update DAY5.10일차 - 비밀번호.py

--- SWEA/DAY5.10일차 - 비밀번호.py
+++ SWEA/DAY5.10일차 - 비밀번호.py
@@ -2,7 +2,7 @@ for tc in range(1, 11) : # 1부터 10까지의 테스트 케이스
   N, arr = input().split() # 문자열 개수, 문자열 값
   arr = list(arr) # 리스트로 설정
   
-  s = 0 #
+  s = 0 # 인덱스 초기값 설정
   while s < len(arr) : # 처음부터 끝까지 반복 수행
     if s and arr[s] == arr[s-1] : # 앞과 뒤의 문자열 비교
       del arr[s], arr[s-1]  # 같으면 앞과 뒤의 문자열 삭제
