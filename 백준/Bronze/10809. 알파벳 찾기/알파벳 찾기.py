bf2006c07d131fac3969f26c93a25ff16edf951a
Jun <122006463+TiranoPower@users.noreply.github.com>
2023-02-11 18:39:07 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,20 @@
+import sys
+
+
+n = sys.stdin.readline().strip()
+
+alaph = list(range(97,123))
+
+for i in alaph :
+    print(n.find(chr(i)), end = ' ')
+
+# b : 98 a : 97 e: 100 k : 106 j : 107 o : 110 o : 110 n : 119
+
+
+ # 1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+        
+
+
+    
+
+
