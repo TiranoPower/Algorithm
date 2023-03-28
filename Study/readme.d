0fdb009f1812227752a35b9de923b16f6e40d5df
Jun <122006463+TiranoPower@users.noreply.github.com>
2023-03-29 01:34:00 +0900

    Update readme.d

--- Study/readme.d
+++ Study/readme.d
@@ -1,1 +1,21 @@
+이것이 취업을 위한 코딩테스트다
 
+1. 파이썬 문법 : https://www.youtube.com/watch?v=m-9pAwq1o3w&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=1
+
+2. 그리디 & 구현 : youtube.com/watch?v=2zjoKjt97vQ&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=2
+
+3. DFS & BFS : https://www.youtube.com/watch?v=7C9RgOcvkvo&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=3
+
+4.정렬 알고리즘 : https://www.youtube.com/watch?v=KGyK-pNvWos&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=4'
+
+5.이진탐색 : https://www.youtube.com/watch?v=94RC-DsGMLo&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=6
+
+6.다이나믹 프로그래밍 : https://www.youtube.com/watch?v=5Lu34WIx2Us&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=6
+
+7.최단경로 알고리즘 :  https://www.youtube.com/watch?v=acqm9mM1P6o&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=7
+
+8. 기타 그래프 이론 : https://www.youtube.com/watch?v=aOhhNFTIeFI&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=8
+
+9. 기타 알고리즘 : https://www.youtube.com/watch?v=cswJ1h-How0&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=9
+
+10. 개발형 코딩테스트 :https://www.youtube.com/watch?v=d8KPN79UAKA&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=10
